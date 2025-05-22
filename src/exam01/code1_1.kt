a5d689333f2febf2f2b1707b76c0e4d074a63ba3
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-05-22 12:37:45 +0900

    練習

--- src/exam01/code1_1.kt
+++ src/exam01/code1_1.kt
@@ -6,7 +6,7 @@ fun main() {
         val result = if (it % 15 == 0) {
             "FizzBuzz"
         } else if (it % 5 == 0) {
-            "Buzz"  // ← 修正
+            "Buzz"
         } else if (it % 3 == 0) {
             "Fizz"
         } else {
