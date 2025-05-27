0789e3503a7c7ad2b5b99d5805beb8dd772dea74
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-05-27 19:45:31 +0900

    commit

--- src/exam02/code2_1.kt
+++ src/exam02/code2_1.kt
@@ -1,35 +1,16 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 package exam02
 import kotlin.random.Random
 
 fun main() {
 
-
-
-
-
-
-
-
 }
 
 class Dice() {
     var number: Int = 1  // 出目の初期値
-
+}
     fun roll() {
+
+    }
 
 
 
