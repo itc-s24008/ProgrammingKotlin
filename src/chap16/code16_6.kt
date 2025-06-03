205ea105d5d66a353b41ce3c98566ffaa3bc7342
itc-s24008 <itc-s24008@std.it-college.ac.jp>
2025-06-03 15:03:17 +0900

    １６章

--- src/chap16/code16_6.kt
+++ src/chap16/code16_6.kt
@@ -1,9 +1,10 @@
 package chap16
 
 fun main() {
-    val a: IntArray = intArrayOf(1000, 1200, 1500, 1000, 2400)
-    for ((index, money) in a.withIndex()) {
-        println("${index + 1}年目の時給は、${money}円です。")
-
+    val a: IntArray = intArrayOf(
+        1000, 1200, 1500, 1800, 2400, 3000, 3800
+    )
+    for (money in a) {
+        println("時給は ${money}円です。")
     }
 }
